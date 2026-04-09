37acefd26ca8681077989ab49db7d66385a4ba1d
oscarmate936 <oscarmate936@gmail.com>
2026-04-08 22:26:35 -0600

    Eliminar constants.py

--- constants.py
+++
@@ -1,6 +0,0 @@
-# constants.py
-import streamlit as st
-
-# Obtener API Key desde los secretos de Streamlit
-API_KEY = st.secrets["API_KEY"]
-BASE_URL = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}/"
